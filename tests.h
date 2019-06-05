5fd5119f760dc71af2f51d7d1bce63e89af01455
Danming Wang <danming@ucsb.edu>
2019-06-04 17:38:58 -0700

    Implemented version of pa02

---
+++ tests.h
@@ -0,0 +1,28 @@
+// test.h
+// Danming Wang
+// 05/04/2019
+
+#ifndef TEST_H
+#define TEST_H
+#include <iostream>
+
+using namespace std;
+
+void runAll();
+void test_constructor();
+void test_printInOrder();
+void test_printPostOrder();
+void test_visitedNode();
+void test_count();
+void test_MaxNode();
+
+void START_TEST(string testname){
+    cout << "Start "<<testname<<endl;
+}
+
+void END_TEST(string testname){
+    cout << "End "<<testname<<endl;
+    cout<<endl;
+}
+
+#endif
